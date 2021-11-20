2370e66d18c44122e5080b9f44d68f1b263fe4c7
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-11-20 19:47:48 +0000

    Created a Pokemon list array with objects using JS

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Squirtle', height: 0.5, types: ['water']},
+    {name: 'Caterpie', height: 0.3, types: ['bug']}
+];
+
+console.log(pokemonList);
