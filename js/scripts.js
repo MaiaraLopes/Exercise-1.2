d74b11e750fc9456b89af463d516f595c2a43a60
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-11-24 15:43:26 +0000

    Improve the if statement making the code shorter and deleted useless comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,18 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types: ['fire']},
-    {name: 'Squirtle', height: 0.5, types: ['water']},
-    {name: 'Caterpie', height: 0.3, types: ['bug']}
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, types: ['fire'] },
+    { name: 'Squirtle', height: 0.5, types: ['water'] },
+    { name: 'Caterpie', height: 0.3, types: ['bug'] }
 ];
 
-for (let i = 0; i < pokemonList.length; i++){
-    //Highlighting Pokémons that are bigger than 0.7
+for (let i = 0; i < pokemonList.length; i++) {
+    //Highlighting Pokémons that are bigger than 0.6
+    
+    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+    
     if (pokemonList[i].height > 0.6) {
-        document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " Wow, that\'s big!");
-    } else {
-        document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+        document.write(" Wow, that\'s big!");
+    
     }
     document.write('<br>');
 }
-
-
-
-
-
-// if (pokemon[i].age <19 && person[i].age >13){  
-// }else if (person[i].age <13){
-//     console.log(person[i].name + " is a child");
-//   }else {
-//     console.log(person[i].name + " is an adult");
-//   }
