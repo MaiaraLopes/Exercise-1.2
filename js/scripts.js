c0eba5423a2a5798973bed2399c47de32098937e
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-11-27 16:09:58 +0000

    Added pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, types: ['fire'] },
-    { name: 'Squirtle', height: 0.5, types: ['water'] },
-    { name: 'Caterpie', height: 0.3, types: ['bug'] }
-];
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, types: ['fire'] },
+        { name: 'Squirtle', height: 0.5, types: ['water'] },
+        { name: 'Caterpie', height: 0.3, types: ['bug'] }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
 
 //Highlighting Pokémons taller than 0.6
 function myList(pokemon) {
@@ -14,4 +31,4 @@ function myList(pokemon) {
     document.write('<br>');
 
 }
-pokemonList.forEach(myList);
+pokemonRepository.getAll().forEach(myList);
