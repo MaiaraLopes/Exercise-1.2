837700462704394c9bbebddf15d076267e508f18
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-12-22 16:26:12 +0000

    Updated CSS and HTML to remove old  modal content and cleaned up JS with Bootstrap removing old modal content as well

--- js/scripts.js
+++ js/scripts.js
@@ -51,8 +51,7 @@ let pokemonRepository = (function () {
             item.imageUrlFront = details.sprites.front_default;
             item.imageUrlBack = details.sprites.back_default;
             item.height = details.height;
-            item.types = details.types;
-        }).catch(function (e) {
+            }).catch(function (e) {
             console.error(e);
         });
     }
@@ -64,82 +63,29 @@ let pokemonRepository = (function () {
         
     }
 
-    //let modalContainer = document.querySelector('#modal-container');
-
     function showModal(pokemon) {
         let modalBody = $('.modal-body');
         let modalTitle = $('.modal-title');
-        let modalHeader = $('.modal-header');
     
     modalTitle.empty();
     modalBody.empty();
 
     //Add new modal content
 
-    let nameElement = $('<h1>' + pokemon.name + '</h1>');
+    let nameElement = $('<h1 class = "text-capitalize">' + pokemon.name + '</h1>');
     let imageElementFront = $('<img class="modal-img" style="width:50%">');
     imageElementFront.attr("src", pokemon.imageUrlFront);
     let imageElementBack = $('<img class="modal-img" style="width:50%">');
     imageElementBack.attr("src", pokemon.imageUrlBack);
-    let heightElement = $("<p>" + "height: " + pokemon.height * 10 + " " + "cm" + "</p>");
-
+    let heightElement = $("<p>" + "Height: " + pokemon.height * 10 + " " + "cm" + "</p>");
+    
     modalTitle.append(nameElement);
     modalBody.append(imageElementFront);
     modalBody.append(imageElementBack);
     modalBody.append(heightElement);
 
     }
-    /*
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1')
-    titleElement.innerText = title;
-
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    //Add Pokémon image to modal
-
-    let imageContainer = document.querySelector('#image-container');
-    let imageElement = document.createElement('img');
-    imageElement.src = image;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modal.appendChild(imageElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-}
-
-let dialogPromiseReject;
-
-function hideModal() {
-    modalContainer.classList.remove('is-visible');
-        
-    if (dialogPromiseReject) {
-        dialogPromiseReject();
-        dialogPromiseReject = null;
-    }
-}
-
-window.addEventListener('keydown', function(e) {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-        hideModal ();
-    }
-});
-
-modalContainer.addEventListener('click', function(e) {
-    let target = e.target;
-    if (target === modalContainer) {
-        hideModal();
-    }
-});*/
-
+    
 return {
         add: add,
         getAll: getAll,
